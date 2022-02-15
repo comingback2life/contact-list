346ee1fe9354420ef3af5687e2244fb788f7d2f9
Samip <callmesamip@gmail.com>
2022-02-15 14:26:37 +1100

    User fetch complete

---
+++ script.js
@@ -0,0 +1,41 @@
+const apiURL= "https://randomuser.me/api/?results=20";
+const listElm = document.getElementById('list');
+
+const fetchUser=()=>{
+ fetch(apiURL)
+ .then(res=>res.json())
+ .then(data=>{
+   let str="";
+   const users=data.results;
+   users.map(users=>{
+    str+=`
+    <div class="col-md-6 col-lg-3 py-3">
+    <div class="card h-100" style="width: 20rem;">
+      <img src="${users.picture.large}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${users.name.title}. ${users.name.first} ${users.name.last}</h5>
+        <p class="card-text">
+        <ul class="list-unstyled">
+        <li> <i class="fa-solid fa-phone text-center pt-2 mt-2"></i>
+        ${users.phone}</li>
+        <li>
+        <i class="fa-solid fa-envelope text-center pt-2 mt-2"></i>${users.email}</li>
+        <li><i class="fa-solid fa-location-dot text-center pt-2 mt-2"></i> ${users.location.city} ${users.location.state} ${users.location.postcode} , ${users.location.country} </li>
+        </ul>
+        </p>
+      </div>
+    </div>
+  </div>
+    `
+   })
+
+   listElm.innerHTML=str;
+ });
+};
+fetchUser();
+
+
+const handleOnChange = e=>{
+  const value = e.target.value;
+  console.log(value);
+}
