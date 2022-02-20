28af2cf6bc37b3cac3061f5970cdbb5e7efb7556
Samip <callmesamip@gmail.com>
2022-02-21 09:09:36 +1100

    Added live search feature and alert in the index page

--- script.js
+++ script.js
@@ -35,29 +35,32 @@ const fetchUser = (params = "results=12") => {
 };
 fetchUser();
 
-const displayCard = (users="Not found") => {
+const displayCard = (users) => {
   let str="";
   if(users){
-    str+=`
-    <div class="col-md-6 col-lg-3 py-3">
-    <div class="card h-100" style="width: 20rem;">
-      <img src="${users.picture.large}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${users.name.title}. ${users.name.first} ${users.name.last}</h5>
-        <p class="card-text">
-        <ul class="list-unstyled">
-        <li> <i class="fa-solid fa-phone text-center pt-2 mt-2"></i>
-        ${users.phone}</li>
-        <li>
-        <i class="fa-solid fa-envelope text-center pt-2 mt-2"></i>${users.email}</li>
-        <li><i class="fa-solid fa-location-dot text-center pt-2 mt-2"></i> ${users.location.city} ${users.location.state} ${users.location.postcode} , ${users.location.country} </li>
-        </ul>
-        </p>
+    users.forEach(e=>{
+      str+=`
+      <div class="col-md-6 col-lg-3 py-3">
+      <div class="card h-100" style="width: 20rem;">
+        <img src="${e.picture.large}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${e.name.title}. ${e.name.first} ${e.name.last}</h5>
+          <p class="card-text">
+          <ul class="list-unstyled">
+          <li> <i class="fa-solid fa-phone text-center pt-2 mt-2"></i>
+          ${e.phone}</li>
+          <li>
+          <i class="fa-solid fa-envelope text-center pt-2 mt-2"></i>${e.email}</li>
+          <li><i class="fa-solid fa-location-dot text-center pt-2 mt-2"></i> ${e.location.city} ${e.location.state} ${e.location.postcode} , ${e.location.country} </li>
+          </ul>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-    `
-    listElm.innerHTML=str;
+      `
+      listElm.innerHTML=str;
+    })
+   
   }
 };
 
@@ -68,12 +71,17 @@ const handleOnChange = e => {
 
 const handleOnType = e => {
   if(e.value===""){
-   fetchUser();
+   let str=`<div class="alert alert-danger" role="alert">
+   The search bar is empty, my friend !
+ </div>`
+ listElm.innerHTML=str;
   }
-  let user=""
+  let user=[];
   const foundUser = users.filter((item) => {
-    if (item.name.first === e.value) {
-      user = item;
+   // item?.name.first //nullish operator
+    const userName = `${item.name.first} ${item.name.last}`.toLowerCase();
+    if (userName.includes(e.value.toLowerCase())) { 
+      user.push(item);
       displayCard(user);
     }
   }
